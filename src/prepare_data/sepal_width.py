60f3359b908d1222b892f6485342abecc5efad96
Khinevich Aleksandr <a.khinevich@cft.ru>
2023-05-20 15:33:11 +0300

    add new feature

---
+++ src/prepare_data/sepal_width.py
@@ -0,0 +1,23 @@
+import os
+import sys
+from typing import Any
+import pandas as pd
+import numpy as np
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from wrapper import feature_wrapper  # noqa E402
+
+
+def update_features(x: float):
+    return np.log10(x + x)
+
+
+@feature_wrapper
+def sepal_width(df: pd.DataFrame | None = None,
+                params: dict[str, Any] | None = None) -> pd.DataFrame:
+    df[params.get('feature')] = df[params.get('feature')].fillna(0)
+    df[params.get('feature')] = df[params.get('feature')].map(update_features)
+    return df
+
+
+if __name__ == "__main__":
+    sepal_width()
